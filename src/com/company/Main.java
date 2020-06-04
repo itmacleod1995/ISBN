a496487f84666f10f5e29f73d7382a60ccd91a9b
itmacleod1995 <imacleod@mail.umw.edu>
2020-06-03 21:06:29 -0400

    Prints out what the code is

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,6 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	        System.out.println("This is a ISBN Verifier");
+	    System.out.print("This is an ISBN Verifier");
     }
 }
