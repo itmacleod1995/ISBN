35142ad82073a491bd5231d075d678b6dbf79649
itmacleod1995 <imacleod@mail.umw.edu>
2020-06-03 21:08:19 -0400

    Added Scanner class

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,11 @@
 package com.company;
-
+import java.util.*;
 public class Main {
 
     public static void main(String[] args) {
+        Scanner stdin = new Scanner(System.in);
 	    System.out.print("This is an ISBN Verifier");
+
+
     }
 }
